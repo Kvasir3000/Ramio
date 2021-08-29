75c8c6e72d46c676906efba3acc98b2ae0e5608c
Nikita Kishore <kvasir3000@gmail.com>
2021-08-29 21:35:18 +0300

    Add backround picture

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame as pg
+import pygame.image
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 1920, 1080
 MAIN_WINDOW = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+BACKGROUND = pygame.image.load('farm.png')
 
 def handle_event(user_event):   # TODO: try to use match-case statement
     if user_event.type == pg.QUIT:
@@ -14,3 +15,6 @@ pg.init()
 while pg.get_init():
     for event in pg.event.get():
         handle_event(event)
+
+    MAIN_WINDOW.blit(BACKGROUND, (0, 0))
+    pygame.display.update()
