fc2d7f8b1a13cce272a9ba83ad5559860b0f23e0
tima <timathevedmed@gmail.com>
2021-08-31 22:36:14 +0300

    Refactor game object

--- game_object.py
+++ game_object.py
@@ -1,34 +1,12 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from pygame.image import load
 from pygame.sprite import Sprite
-import pygame
-# class GameObject(ABC):
-#     def __init__(self, pos_x, pos_y, *sprites):  # TODO: check is sprites are empty
-#         self.sprite_idx = 0
-#         self.sprites = list(sprites)
-#         self.image = self.sprites[self.sprite_idx]
-#
-#         self.rect = self.image.get_rect()
-#         self.rect.topleft = [pos_x, pos_y]
-#
-#     @abstractmethod
-#     def update(self):
-#         pass
-#
-#     @abstractmethod
-#     def draw(self):
-#         pass
-#
-#     @staticmethod
-#     def collision(obj1, obj2):
-#         return obj1.rect.colliderect(obj2)
 
 
-class GameObject(Sprite):
-    def __init__(self, x, y, image=''):
-        pygame.sprite.Sprite.__init__(self)
-        if image != '':
-            self.image = pygame.image.load(image)
-            self.rect = self.image.get_rect(center=(x, y))
-        else:
-            self.image = pygame.Surface([0, 0])
-            self.rect = self.image.get_rect()
+class GameObject(Sprite, ABC):
+    def __init__(self, x, y, *images):
+        super().__init__()
+        self.images = [load(image) for image in images]
+        self.image_idx = 0
+        self.image = self.images[self.image_idx]
+        self.rect = self.image.get_rect(bottomleft=(x, y))
