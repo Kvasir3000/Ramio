b372b01e27f61f63ce23e3ba6803ea80f13b51fa
tima <timathevedmed@gmail.com>
2021-08-31 23:24:57 +0300

    Refactor player class

--- player.py
+++ player.py
@@ -1,49 +1,21 @@
 from game_object import GameObject
-from pygame import image
-
-
-# class Player(GameObject):
-#     def __init__(self, pos_x, pos_y):
-#         super().__init__(pos_x, pos_y, image.load('ghost.png'))
-#
-#     def update(self):
-#         pass
-#
-#     def move_left(self):
-#         self.rect.move_ip(-25, 0)
-#
-#     def move_right(self):
-#         self.rect.move_ip(25, 0)
-#
-#     def draw(self):
-#         pass
+from glob import glob
 
 
 class Player(GameObject):
+    __DEFAULT_VELOCITY = 1
+    # TODO: make x and y as pair
+
     def __init__(self, x, y):
-        super().__init__(x, y, 'Character01.png')
-        self.velocity = 1
-        self.current_sprite = 0
-        self.sprites = []
-        self.sprites.append(image.load('Character01.png'))
-        self.sprites.append(image.load('Character02.png'))
-        self.sprites.append(image.load('Character03.png'))
-        self.sprites.append(image.load('Character04.png'))
-        self.sprites.append(image.load('Character05.png'))
-        self.sprites.append(image.load('Character06.png'))
+        super().__init__(x, y, self.__DEFAULT_VELOCITY, "Character01.png")
 
     def update(self, *args):
         if args[0] == 1:
-            self.current_sprite += 0.09
-            if self.current_sprite >= len(self.sprites):
-                self.current_sprite = 0
-            self.image = self.sprites[int(self.current_sprite)]
+            self.image_idx = (self.image_idx + 0.09) % len(self.images)
+            self.image = self.images[int(self.image_idx)]
             self.rect.move_ip(self.velocity, 0)
         elif args[0] == -1:
             self.rect.move_ip(-self.velocity, 0)
         else:
             current_sprite = 0
-            self.image = self.sprites[current_sprite]
-
-    def __call__(self, *args, **kwargs):
-        pass
+            self.image = self.images[current_sprite]
